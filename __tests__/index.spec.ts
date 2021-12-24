298ea7b5da16dd82d767d292e04b7d0bfb09a532
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2021-12-24 09:29:44 +0100

    feat: setup jest for project

---
+++ __tests__/index.spec.ts
@@ -0,0 +1,10 @@
+import getEnvVar from '../src';
+
+
+describe('@bolajiolajide/env-utils', () => {
+  test('expect true to be true', (done) => {
+    expect(true).toBe(true);
+
+    done();
+  });
+});
