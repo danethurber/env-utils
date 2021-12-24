e34c6e1af6cbb5384f501b8f1d4a40bdac57427e
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2021-12-24 09:20:44 +0100

    feat: rewrite env-utils in ts

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import type { EnvOptions, EnvValue } from './types';
+
+
+const trimElements = (element: string) => element.trim();
+
+const getEnvVar = (key: string, options: EnvOptions): EnvValue | EnvValue[] => {
+  const fallback = process.env['NODE_ENV'] === 'development' ? options.devDefault : undefined;
+  const envValue = process.env[key] === undefined ? fallback : process.env[key];
+
+  const isOptionalAndUndefined = !options.optional && typeof envValue === 'undefined';
+  const isUndefined = typeof envValue === 'undefined';
+  const isString = typeof envValue === 'string';
+
+  if (isOptionalAndUndefined || isUndefined) {
+    throw new Error(`${key} is undefined`);
+  }
+
+  if (options.isBoolean && isString) {
+    return envValue === 'true';
+  }
+
+  if (options.commaSeparated && isString) {
+    return envValue.split(',').map(trimElements);
+  }
+
+  if (options.isNumber && isString) {
+    return parseInt(envValue, 10);
+  }
+
+  return envValue;
+};
+
+export default getEnvVar;
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export type EnvValue = string | number | boolean;
+
+export interface EnvOptions {
+  devDefault: EnvValue | EnvValue[];
+  isBoolean: boolean;
+  optional: boolean;
+  commaSeparated: boolean;
+  isNumber: boolean;
+}
